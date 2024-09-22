6828bd8a4fdef7486706cc526af9d5ecd0f5abcc
subbu999 <b.subbu504@gmail.com>
2024-09-22 18:24:54 +0530

    Home Page Completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
+import { Router, Event, NavigationEnd } from '@angular/router';
+import { ViewportScroller } from '@angular/common';
+import * as AOS from 'aos';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -6,7 +9,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit {
   title = 'CPS';
+  loading: boolean = true;
+  windowScrolled: boolean = false;
+  @HostListener('window:scroll', []) onWindowScroll() {
+    if (window.scrollY > 100) {
+      this.windowScrolled = true;
+    }
+    else if (this.windowScrolled && window.scrollY < 10) {
+      this.windowScrolled = false;
+    }
+  }
+  constructor(private router: Router, private viewPortScroller: ViewportScroller) {
+    this.router.events.subscribe((event: Event) => {
+      if (event instanceof NavigationEnd) {
+        this.viewPortScroller.scrollToPosition([0, 0]);
+      }
+    });
+  }
   ngOnInit() {
-
+    AOS.init({
+      duration: 1000
+    });
+    setTimeout(() => {
+      this.loading = false;
+    }, 1500)
+  }
+  onScrollTopClick() {
+    window.scroll({
+      top: 0,
+      left: 0,
+      behavior: 'smooth'
+    })
   }
 }
